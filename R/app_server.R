689461479df9eb07b5dfaa1228cf2b8e54f5e039
jennathor <82481199+jennathor@users.noreply.github.com>
2024-06-14 16:22:35 -0500

    Update 'About' Tab
    ui:
    - simplify it by putting FAQ and Version info in the fct_helpers file
    - add comments to organize main panel's tabs
    
    server:
    - add FAQ section and site updates table to support ui

--- R/app_server.R
+++ R/app_server.R
@@ -3010,6 +3010,28 @@ app_server <- function(input, output, session) {
 
   })
 
+  # 'About' tab FAQ's and answers
+  output$faq_list <- renderUI({
+    faq_items <- lapply(seq_len(nrow(faqs)), function(i) {
+      tags$div(
+        class = "faq-item",
+        tags$h5(
+          class = "faq-question",
+          faqs$question[i]
+        ),
+        tags$p(
+          class = "faq-answer",
+          faqs$answer[i]
+        )
+      )
+    })
+    tagList(faq_items)
+  })
+
+  # 'About' tab Site Updates table
+  output$site_updates_table <- renderTable({
+    site_updates_df
+  }, striped = TRUE)
 
 #
 #  Python
