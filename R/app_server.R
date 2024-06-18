d0b5ad3fdef5ef8fc4bfdd9006a2e42cfc0c5099
dan-burk <108594996+dan-burk@users.noreply.github.com>
2024-06-18 16:32:16 -0500

    Change renderUI to renderText

--- R/app_server.R
+++ R/app_server.R
@@ -242,7 +242,7 @@ app_server <- function(input, output, session) {
   # showing the current dataset. Warning if no is uploaded.
 
     
-  output$selected_dataset <- renderUI({
+  output$selected_dataset <- renderText({ #renderUI
       req(input$submit_button)
       # when submit is clicked, but no data is uploaded -- Not Applicable in HMCL
       # When submit is clicked, and data is uploaded -- Not Applicable in HMCL
@@ -811,7 +811,7 @@ app_server <- function(input, output, session) {
             )
 
             selected_file <- response$choices$message.content
-
+            
             tem1 <- gsub("\\..*", "", selected_file)
             tem2 <- gsub("_", " ", tem1)
             selected_data_file(tem2)
