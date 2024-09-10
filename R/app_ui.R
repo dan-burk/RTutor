2fbc2ce2e48fe9616dbc2e23ce9347cff51e30d6
Jenna Thorstenson <jennathor974@gmail.com>
2024-09-10 15:12:17 -0500

    Create CSS style module
    - also updated the site title version to change as the rtutor version changes

--- R/app_ui.R
+++ R/app_ui.R
@@ -1260,7 +1260,7 @@ golem_add_external_resources <- function() {
     ),
     bundle_resources(
       path = app_sys("app/www"),
-      app_title = "RTutor 0.99"
+      app_title = paste("RTutor", release)
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
